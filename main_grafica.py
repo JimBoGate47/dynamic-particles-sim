d4bf75bb41b21842eaf7f2fdd141419f17c43113
jimbo <jimbo.gate47@gmail.com>
2025-09-06 17:31:55 -0400

    chore: rename script

--- main_grafica.py
+++ main_grafica.py
@@ -1,19 +1,27 @@
+import warnings
+
 import pandas as pd
 import torch
 
-from backend.src.simulator.domain.entities.particle_system import ParticleSystem2D
-from backend.src.common.domain.types.properties import SimulationProperties, PhysicalProperties
+from backend.src.common.domain.entities.properties import SimulationProps, PhysicalProps
+from backend.src.simulator.domain.entities.particle_system import ParticleSystem2DTensor
 from plotting.plot_particles2 import plot_data
+
+warnings.warn(
+    "This function is deprecated. Please use `cli.py` and `cli_plot.py` instead.",
+    DeprecationWarning,
+)
 
 RADIO = 6.0
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-ps = ParticleSystem2D(
-    pos=ParticleSystem2D.initialize_particles_in_circle(
+ps = ParticleSystem2DTensor(
+    pos=ParticleSystem2DTensor.initialize_particles_in_circle(
         n_particles=64,
         R=RADIO,
         device=device,
     ),
-    sim_props=SimulationProperties(
+    # sim_props=SimulationProperties(
+    sim_props=SimulationProps(
         g=9.81,
         k=9e9,
         min_vel=1e3,
@@ -21,7 +29,7 @@ ps = ParticleSystem2D(
         beta=0.5,
         dt=0.1,
     ),
-    phys_props=PhysicalProperties(
+    phys_props=PhysicalProps(
         q=1.0,
         m=1.0,
     )
@@ -33,6 +41,7 @@ for i in range(1000):
     evolucion += ps.to_dict
 
 res_df = pd.DataFrame(evolucion)
+print(res_df.head())
 plot_data(res_df, x="rx", y="ry", animation_frame="step", hover_name="step",
           # range_x=[-1.2, 1.2],
           # range_y=[-1.2, 1.2],
